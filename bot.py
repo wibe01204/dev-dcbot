ad60a956427c8850b7dc6b930180344cd1efcb87
wibe01204 <90883053+wibe01204@users.noreply.github.com>
2021-09-18 13:25:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,12 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     print('Bot is ready')
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} 加入了伺服器')
+
+@client.event
+async def on_member_remover(member):
+    print(f'{member} 退出了了伺服器')
+
 client.run('ODg4MjcyNzMzMjUzODEyMjQ0.YUQScg.V5BBm3wfLafT6hmQN0QWI4Da34Q')
