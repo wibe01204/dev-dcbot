2c9c5c48c35a0a046de860ad880da298db66bed8
wibe01204 <90883053+wibe01204@users.noreply.github.com>
2021-09-19 01:59:46 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,12 +24,4 @@ async def _8ball(ctx, *, question):
 async def clear(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
 
-@client.event
-async def on_member_join(member):
-    print(f'{member} 加入了伺服器')
-
-@client.event
-async def on_member_remover(member):
-    print(f'{member} 退出了了伺服器')
-
-client.run('ODg4MjcyNzMzMjUzODEyMjQ0.YUQScg.V5BBm3wfLafT6hmQN0QWI4Da34Q')
+client.run('ODg4MjUxMDc3MDI2MjY3MTc2.YUP-Rw.2X53VO2HtucTgPf-1nOw4JnavU0')
