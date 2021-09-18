191589c46ace3b9112aff89892ecc52aa8ee1adf
wibe01204 <90883053+wibe01204@users.noreply.github.com>
2021-09-18 23:48:37 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 import random
+from discord import message
+from discord import voice_client
 from discord.ext import commands
+from discord.ext.commands import bot
 
 client = commands.Bot(command_prefix='!a')
 
@@ -12,7 +15,7 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'Ping: {round(client.latency * 1000)}ms')
 
-@client.command(aliases=['8ball', ])
+@client.command(aliases=['choose'])
 async def _8ball(ctx, *, question):
     responses = ['要','不要']
     await ctx.send(f'問題: {question}\n回答: {random.choice(responses)}')
