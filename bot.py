3a2d70089ce7a85a151a4f9933b43741ec9b4b21
wibe01204 <90883053+wibe01204@users.noreply.github.com>
2021-09-20 00:46:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import random
 from discord import message
 from discord import voice_client
+from discord import channel
 from discord.ext import commands
 from discord.ext.commands import bot
 
@@ -9,7 +10,7 @@ client = commands.Bot(command_prefix='!a')
 
 @client.event
 async def on_ready():
-    print('Bot is ready')
+    print('機器人已上線')
 
 @client.command()
 async def ping(ctx):
@@ -23,5 +24,6 @@ async def _8ball(ctx, *, question):
 @client.command()
 async def clear(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
+    await ctx.send(f'已移除10則訊息!!')
 
 client.run('ODg4MjUxMDc3MDI2MjY3MTc2.YUP-Rw.2X53VO2HtucTgPf-1nOw4JnavU0')
