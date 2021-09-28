2cdeae36bc752f1b073a4c43e766c5e428da6b38
wibe01204 <90883053+wibe01204@users.noreply.github.com>
2021-09-28 14:51:28 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from discord import voice_client
 from discord import channel
 from discord.ext import commands
 from discord.ext.commands import bot
+from discord.utils import get
 
 client = commands.Bot(command_prefix='!a')
 
@@ -29,5 +30,38 @@ async def clear(ctx, amount=6):
 @client.command()
 async def rn(ctx):
     await ctx.send(f'0~100中我選擇的隨機號碼為: {random.randint(0,100)}')
+
+@client.command(pass_context=True, aliases=['j'])
+async def join(ctx):
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f'Miyuki加入到 >> {channel} <<\n')
+
+    await ctx.sent(f'Miyuki加入到 >> {channel} <<')
+
+@client.command(pass_context=True, aliases=['l'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f'Miyuki離開了 >> {channel} <<')
+        await ctx.send(f'Miyuki離開了 >> {channel} <<')
+    else:
+        print('Miyuki離開了')
+        await ctx.send(f'Miyuki離開了頻道')
+
 
 client.run('ODg4MjUxMDc3MDI2MjY3MTc2.YUP-Rw.2X53VO2HtucTgPf-1nOw4JnavU0')
