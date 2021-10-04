09c092b5239110523d44f64a87bcae064d252fa3
wibe01204 <90883053+wibe01204@users.noreply.github.com>
2021-10-04 10:33:39 +0800

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,101 @@
+/**
+ * Module Imports
+ */
+const { Client, Collection } = require("discord.js");
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { TOKEN, PREFIX } = require("./util/Util");
+const i18n = require("./util/i18n");
+
+const client = new Client({
+  disableMentions: "everyone",
+  restTimeOffset: 0
+});
+
+client.login(TOKEN);
+client.commands = new Collection();
+client.prefix = PREFIX;
+client.queue = new Map();
+const cooldowns = new Collection();
+const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+/**
+ * Client Events
+ */
+client.on("ready", () => {
+  console.log(`${client.user.username} 上線!`);
+  client.user.setActivity(`${PREFIX}help/請多多指教`, { type: "LISTENING" });
+});
+client.on("warn", (info) => console.log(info));
+client.on("error", console.error);
+
+/**
+ * Import all commands
+ */
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(join(__dirname, "commands", `${file}`));
+  client.commands.set(command.name, command);
+}
+
+client.on("message", async (message) => {
+    // 如果訊息的內容是 '早安'
+    if (message.content.includes('早安')) {
+        // 則 Bot 回應 '歐嗨喲~今天也好好努力吧!!'
+        message.channel.send('歐嗨喲~今天也好好努力吧!!');
+    }
+    // 如果訊息的內容是 '晚安'
+    if (message.content.includes('晚安')) {
+        // 則 Bot 回應 '今天的你辛苦了!! 晚安~'
+        message.channel.send('今天的你辛苦了!! 好好休息吧~');
+    }
+    if (message.content.includes('的機率')) {
+        // 則 Bot 回應 '機率!!'
+        message.channel.send((Math.round(Math.random()*101)) + '%'); 
+    }
+  if (message.author.bot) return;
+  if (!message.guild) return;
+
+  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(PREFIX)})\\s*`);
+  if (!prefixRegex.test(message.content)) return;
+
+  const [, matchedPrefix] = message.content.match(prefixRegex);
+
+  const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command =
+    client.commands.get(commandName) ||
+    client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 1) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        i18n.__mf("common.cooldownMessage", { time: timeLeft.toFixed(1), name: command.name })
+      );
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply(i18n.__("common.errorCommand")).catch(console.error);
+  }
+});
