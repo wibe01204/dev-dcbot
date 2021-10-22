7b0f2cefd95ba95ceed0f8e3629141dafa274191
wibe01204 <90883053+wibe01204@users.noreply.github.com>
2021-10-22 10:05:53 +0800

    Update CCXX.py

--- CCXX.py
+++ CCXX.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import random
 
-client = commands.Bot(command_prefix="!a")
+client = commands.Bot(command_prefix="!a",help_command=None)
 
 player1 = ""
 player2 = ""
